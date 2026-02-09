4f2176a72a1da38f5513f393f5bf2d33fecbd5c0
Sargam <sargamkamble4444@gmail.com>
2026-02-09 19:56:45 +0530

    Implement CI/CD pipeline using Jenkins for .NET application

---
+++ Month1/Week4/jenkins/YCCECalc/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace YCCeCalc;
+
+public class MyMaths
+{
+    public int add2nos(int a, int b)
+    {
+        if (a > 0 && b > 0)
+        {
+            return a + b;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        MyMaths obj = new MyMaths();
+        int result = obj.add2nos(5, 3);
+
+        Console.WriteLine("Sum = " + result);
+    }
+}
